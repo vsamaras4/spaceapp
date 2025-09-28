03162fda2c1120c04648dcfa0341eb5317a75c43
vsamaras4 <43777986+vsamaras4@users.noreply.github.com>
2025-09-28 17:44:16 +0300

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'spaceapp' // <-- your repo name
+
+export default {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  // ⬇️ Skip ESLint during `next build`
+  eslint: { ignoreDuringBuilds: true },
+  // (optional) if types ever fail your CI build:
+  // typescript: { ignoreBuildErrors: true },
+}
